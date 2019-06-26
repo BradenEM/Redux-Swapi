340f202b47714895fd9b128dd4ae19f4b6d0c4fb
Braden Mason <bradenemason@gmail.com>
2019-06-25 21:19:13 -0500

    Loading state renders correctly, characters arent yet

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,10 +1,31 @@
-import /* we need our action types here*/ "../actions";
+import { FETCHING, FETCHING_FAILURE, FETCHING_SUCCESS } from "../actions";
 const initialState = {
+  fetching: false,
+  error: "",
   characters: []
   // Array characters, Boolean fetching, null error.
 };
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCHING:
+      return {
+        ...state,
+        fetching: true,
+        error: ""
+      };
+    case FETCHING_SUCCESS:
+      return {
+        ...state,
+        fetching: false,
+        error: "",
+        characters: [...state.characters, action.payload]
+      };
+    case FETCHING_FAILURE:
+      return {
+        ...state,
+        fetching: false,
+        error: action.payload
+      };
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
